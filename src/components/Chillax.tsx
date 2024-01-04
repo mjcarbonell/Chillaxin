d3383d4dba2889e81ba2de83f14b69c0dbb6baf7
MaximCarbonell <maximcarbonell@gmail.com>
2024-01-03 21:06:08 -0800

    new

---
+++ src/components/Chillax.tsx
@@ -0,0 +1,25 @@
+// pages/chillax.js
+import Head from 'next/head';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// Import your custom CSS here
+
+const Chillax = () => {
+  // You can manage state and functionality here
+
+  return (
+    <>
+      <Head>
+        <title>Chillax</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {/* Add other head tags here */}
+      </Head>
+      <div className="section" id="vue-target">
+        {/* Your page content goes here */}
+        
+      </div>
+    </>
+  );
+};
+
+export default Chillax;
