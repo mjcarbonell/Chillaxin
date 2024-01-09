4dafc08f06bd29343f0812506d7ed34e94816a4f
MaximCarbonell <maximcarbonell@gmail.com>
2024-01-08 18:56:29 -0800

    cors

--- next.config.js
+++ next.config.js
@@ -1,12 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { appDir: true, serverComponentsExternalPackages: ["mongoose"] , typedRoutes: true},
+  experimental: {
+    appDir: true, 
+    serverComponentsExternalPackages: ["mongoose"],
+    typedRoutes: true
+  },
   webpack(config) {
-      config.experiments = { ...config.experiments, topLevelAwait: true };
-      return config;
+    config.experiments = { ...config.experiments, topLevelAwait: true };
+    return config;
   },
   images: {
     domains: ['firebasestorage.googleapis.com'],
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*", // Adjust the path according to your API routes
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Use specific domain here for better security
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ],
+      },
+    ]
   },
 }
 
